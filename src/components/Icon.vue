72deb019c5d5c6c0f6a82357b2a1290c045e1454
ddb324 <jiangdaoran@163.com>
2020-09-23 14:08:40 +0800

    将Icon作为全局组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <svg class="icon">
+      <use :xlink:href="'#'+name"></use>
+    </svg>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+@Component
+export default class Icon extends Vue {
+  @Prop(String) readonly name!: string;
+}
+</script>
+
+<style lang='scss' scoped>
+.icon {
+  border-radius: 40%;
+  padding: 2px;
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
